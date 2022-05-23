98a40ee8f70df5bde7631db5669929dd4b28c298
npetitjean <npetitjean@awegroupe.be>
2022-05-23 13:10:19 +0200

    correction

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
-const express = require('express') const app = express() app.get('/', (_req, res) => {
+const express = require('express') 
+const app = express() 
+
+app.get('/', (_req, res) => {
     res.status(200).send('Hello world')
-}) app.get('/test', (_req, res) => {
+}) 
+
+app.get('/test', (_req, res) => {
     res.status(200).send('Test page')
-}) module.exports = app
+}) 
+
+module.exports = app
